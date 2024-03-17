84c83f078a3bc287a86c36beee410db84bd70005
gersooonn <128495179+gersooonn@users.noreply.github.com>
2024-03-16 22:02:16 -0300

    mudando a versao da api do stripe

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2022-11-15",
+  apiVersion: "2023-08-16",
 });
 
 export const POST = async (req: Request) => {
